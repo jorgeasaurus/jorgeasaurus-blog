b7a830b584af06e9e40c00dd92ed4a2e3e56ae3b
jorgeasaurus <j0rg3@me.com>
2026-05-05 17:58:43 -0500

    Refactor App component structure: move Analytics component to render after Routes for improved layout consistency.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import Home from './pages/Home.tsx'
 import Post from './pages/Post.tsx'
 import About from './pages/About.tsx'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/post/:slug" element={<Post />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/:slug" element={<Post />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/post/:slug" element={<Post />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/:slug" element={<Post />} />
+        </Routes>
+      </BrowserRouter>
+      <Analytics />
+    </>
   )
 }
 
